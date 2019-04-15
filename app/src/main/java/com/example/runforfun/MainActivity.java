d9d47f640d2d445259370720d42bdc307ff892fa
Sergey Shevchenko <dragongrox7@gmail.com>
2019-04-16 00:15:54 +0200

    implementacion de GPS

--- app/src/main/java/com/example/runforfun/MainActivity.java
+++ app/src/main/java/com/example/runforfun/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.runforfun;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,6 +12,8 @@ import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -193,6 +197,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 22: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
     /**
      * Etapa del ciclo de la vida que corresponde al inicio de este Activity
      * @param savedInstanceState
@@ -224,6 +253,33 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
 
         FirebaseApp.initializeApp(this);
+
+        //permisos de Localizacion
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        22);
+
+                // ACCESS_FINE_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
 
         //Obtencion del Usuario
         if (user == null) {
@@ -566,6 +622,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intentAniadirAmigo);
     }
 
+    public void OnClicMaps(View view) {
+        Intent intentGoogleMaps = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intentGoogleMaps);
+    }
+
     /**
      * The type Sincronizacion datos async task.
      */
@@ -653,7 +714,7 @@ public class MainActivity extends AppCompatActivity {
                 databaseReferenceUsuario.child("distanciaDia").setValue(0 + "");
                 databaseReferenceUsuario.child("ultimaFecha").setValue(new Usuario().getUltimaFecha());
             }
-            //si el boolean que se encarga de anunciar la eliinacion de un amigo esta en true se procedera a hacer sonar el sonido
+            //si el boolean que se encarga de anunciar la eliminacion de un amigo esta en true se procedera a hacer sonar el sonido
             if (sonido) {
                 ReproducirSonido();
             }
--- app/src/main/java/com/example/runforfun/MapsActivity.java
+++ app/src/main/java/com/example/runforfun/MapsActivity.java
@@ -1,6 +1,12 @@
 package com.example.runforfun;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -9,6 +15,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,9 +45,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addPolyline(new PolylineOptions().add(sydney).add(new LatLng(50, 50)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
